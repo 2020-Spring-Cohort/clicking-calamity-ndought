453ed5ea9f105fafb4aaf0bec9cdf3b096de612e
Noah Doughty <noahdoughty@gmail.com>
2020-03-03 16:56:20 -0500

    Iteration 1
    Created 1st button, button does not have any function yet

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -8,6 +8,12 @@ describe('Clicking Calamity Tests:', () => {
         it('countClick() 1 time should result in a clickCount of 1.', () => {
             underTest.countClick(0);
             expect(underTest.clickCount).toBe(1);
-            });
+         });
+         it('countClick() 2 time will result in a clickCount of 2', () => {
+             underTest.countClick();
+             underTest.countClick();
+             expect(underTest.clickCount).toBe(2);
+         });
+
     })
 });
